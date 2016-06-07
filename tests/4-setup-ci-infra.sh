e2e9a484cfe655d80ebe8871106bef0408163453
Arnaud Lachaume <arnaud.lachaume@maestrano.com>
2016-06-08 08:07:48 +1000

    set configurable wait time for infra specs

--- tests/4-setup-ci-infra.sh
+++ tests/4-setup-ci-infra.sh
@@ -10,9 +10,13 @@ sh ci_environments/aws_ubuntu/scripts/setup_infrastructure.sh
 retval=$?
 
 if [ $retval -eq 0 ]; then
+  # Set wait time (use existing TST_INFRA_WAIT or fallback on default)
+  TST_INFRA_WAIT=${TST_INFRA_WAIT-10} # set wait time (minutes)
+  let "wait_time=$TST_INFRA_WAIT*60" # set wait time (seconds)
+
   # Give some time to the infrastructure to prepare
-  echo "[RUN] sleep for 5 minutes while infrastructure is provisioning"
-  sleep 300
+  echo "[RUN] sleep for $TST_INFRA_WAIT minutes while infrastructure is provisioning"
+  sleep $wait_time
 fi
 
 exit $retval
